706c0e73ff656441ccd1e27e7b4a817b9b12f8c1
yaozuopan <yaozuopan@weuseapp.com>
2021-05-19 12:15:17 +0800

    支持约束

--- MyBase/Sugar.swift
+++ MyBase/Sugar.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 /// 语法糖
 public protocol Sugar { }
@@ -47,6 +48,33 @@ extension Sugar where Self: NSObject {
     @inlinable public static func then(_ instance: Self, sugar: (Self) throws -> Void) rethrows -> Self {
         try sugar(instance)
         return instance
+    }
+}
+
+extension Sugar where Self: UIView {
+    
+    /// 添加约束
+    @inlinable public func make(sugar: (ConstraintMaker) -> Void) {
+        snp.makeConstraints(sugar)
+    }
+    
+    /// 添加约束, 带返回值
+    @inlinable public func thenMake(sugar: (ConstraintMaker) -> Void) -> Self {
+        snp.makeConstraints(sugar)
+        return self
+    }
+    
+    /// 添加约束
+    @inlinable public func make(_ superview: UIView, sugar: (ConstraintMaker) -> Void) {
+        superview.addSubview(self)
+        snp.makeConstraints(sugar)
+    }
+    
+    /// 添加约束, 带返回值
+    @inlinable public func thenMake(_ superview: UIView, sugar: (ConstraintMaker) -> Void) -> Self {
+        superview.addSubview(self)
+        snp.makeConstraints(sugar)
+        return self
     }
 }
 
