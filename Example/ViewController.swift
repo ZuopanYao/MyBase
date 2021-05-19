144a092afbf65dff4ad331d65d259e7a30c1da25
yaozuopan <yaozuopan@weuseapp.com>
2021-05-19 10:24:25 +0800

    fix

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -12,15 +12,5 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let doc = App.Path.document
-//        let path: String = App.Path.document + "/RPReplay_Final1620818994.MP4"
-//        let size = File.Size(path)
-//        let byte: Float = size.value(.byte)
-//        let mb: Float = size.value(.mb)
-
-        print(File(doc + "/政策&协议.rtf").attributedString())
-        
-//        puts(byte, mb)
     }
 }
