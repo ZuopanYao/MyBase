70358edd8be6b3111592477420b12a397a0504d1
yaozuopan <yaozuopan@weuseapp.com>
2021-05-20 20:01:15 +0800

    fix bug

--- MyBase/Global.swift
+++ MyBase/Global.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 /// 全局参数
-struct Global {
+public struct Global {
     
     /// 屏幕的 Size
-    static let size = UIScreen.main.bounds.size
+    public static let size = UIScreen.main.bounds.size
 
     /// 屏幕的宽度
-    static let width = size.width
+    public static let width = size.width
     
     /// 屏幕的高度
-    static let height = size.height
+    public static let height = size.height
     
     /// 如果是 iPad
-    static let isPad = UIDevice.current.userInterfaceIdiom == .pad
+    public static let isPad = UIDevice.current.userInterfaceIdiom == .pad
     
     /// true = 刘海屏
-    static let isNotchScreen: Bool = {
+    public static let isNotchScreen: Bool = {
         guard let keyWindow = App.shared.keyWindow else {
             return false
         }
@@ -31,7 +31,7 @@ struct Global {
     }()
     
     /// true = 黑暗模式
-    static var isDark: Bool {
+    public static var isDark: Bool {
         guard #available(iOS 13.0, *) else { return false }
         return UITraitCollection.current.userInterfaceStyle == .dark
     }
