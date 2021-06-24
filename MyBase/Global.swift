08df59c480f368ab83747895126b7e85bb4406ea
yaozuopan <yaozuopan@weuseapp.com>
2021-06-24 11:26:11 +0800

    fix bug

--- MyBase/Global.swift
+++ MyBase/Global.swift
@@ -11,24 +11,16 @@ import UIKit
 public struct Global {
     
     /// 屏幕的 Size
-    public static let size = UIScreen.main.bounds.size
+    public static var size: CGSize { UIScreen.main.bounds.size }
 
     /// 屏幕的宽度
-    public static let width = size.width
+    public static var width: CGFloat { size.width }
     
     /// 屏幕的高度
-    public static let height = size.height
+    public static var height: CGFloat { size.height }
     
     /// 如果是 iPad
     public static let isPad = UIDevice.current.userInterfaceIdiom == .pad
-    
-    /// true = 刘海屏, Bug: 在根视图的 ViewDidLoad 中使用永远为 false
-//    public static let isNotchScreen: Bool = {
-//        guard let keyWindow = App.shared.keyWindow else {
-//            return false
-//        }
-//        return keyWindow.safeAreaInsets.bottom > 0
-//    }()
     
     /// true = 刘海屏
     public static let isNotchScreen: Bool = {
