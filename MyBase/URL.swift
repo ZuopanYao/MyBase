96cd779f8535f8cc3941a2129797fa058b770a23
yaozuopan <yaozuopan@weuseapp.com>
2021-07-21 18:18:15 +0800

    URL parse parameters

--- MyBase/URL.swift
+++ MyBase/URL.swift
@@ -8,3 +8,23 @@
 import Foundation
 
 // MARK: - Extension URL
+ 
+public extension URL {
+    /// Only web url
+    var parameters: [String: String] {
+        var keyValues: [String: String] = [:]
+        guard let query = query else {
+            return keyValues
+        }
+        query
+            .split(separator: "&")
+            .map { "\($0)" }
+            .forEach {
+                let array = $0.split(separator: "=").map { "\($0)" }
+                guard array.count == 2 else { return }
+                keyValues[array.first!] = array.last!
+            }
+        
+        return keyValues
+    }
+}
